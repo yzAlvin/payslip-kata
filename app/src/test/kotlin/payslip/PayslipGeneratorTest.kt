62a9fe5de0cc6b057736ea4fa29aa7634aec09ac
Fang.Ming <fang.ming@myob.com>
2022-03-23 17:38:12 +0800

    Refactor logic by introducing TaxRange.

--- app/src/test/kotlin/payslip/PayslipGeneratorTest.kt
+++ app/src/test/kotlin/payslip/PayslipGeneratorTest.kt
@@ -3,11 +3,18 @@
  */
 package payslip
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class PayslipGeneratorTest() : DescribeSpec({
     describe("Payslip generator") {
+        it("should throw illegalArgumentException when annual salary is -1") {
+            shouldThrow<IllegalArgumentException> {
+                PayslipGenerator.generate(-1)
+            }
+        }
+
         it("should return payslip with gross income 0, income tax 0, net income 0 when annual salary is 0") {
             PayslipGenerator.generate(0) shouldBe Payslip(0, 0, 0)
         }
